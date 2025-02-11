0896e5438d0aa4f493939806844acd88721ec948
AliceDrop <2353663725@qq.com>
2025-02-11 13:39:33 +0800

    enhence appearence

--- script/basic.js
+++ script/basic.js
@@ -1,3 +1,8 @@
+LINKS = {
+    "通用": "article.html",
+}
+
+
 function initBasicGUI(){
     // 在onload时为网页加上标题栏内容和底部栏内容
     drawTitleBar();
@@ -9,11 +14,11 @@ function drawTitleBar(){
     let nav = document.getElementById("nav");
     nav.innerHTML = `        
         <div id="container" style="margin: 0 auto;display:flex; justify-content:space-between; width:80%;">    
-            <span><img src="../../img/flower_icon.png" alt="icon " height="22px" style="margin:0 5px;"></span>
+            <img src="../../img/flower_icon.png" alt="icon " height="22px" style="margin:0 5px;">
             <span ><a href="/index.html" class="navLink">AliceDrop的笔记共享</a></span>
 
             <span class="navLinkContanier_right">
-                <a href="generalNotes.html" class="navLink">通用</a>
+                <a href="${LINKS["通用"]}" class="navLink">通用</a>
             
                 <a href="/.html" class="navLink">编程语言笔记</a>
 
@@ -26,8 +31,8 @@ function drawTitleBar(){
 
 function drawPageTail(){
     let page_tail = document.getElementById("pageTail");
-    page_tail.innerHTML = `<p style="font-size:15px;">© Copyright AliceDrop 2024-2025  网页内容受 <a class="paraLink" href="https://github.com/Alice-Drop/Coding_Notes_Project/blob/main/LICENSE">CC BY-NC</a> 协议保护。</p>
+    page_tail.innerHTML = `<p style="font-size:15px;">© Copyright AliceDrop 2024-2025  网页内容受 <a href="https://github.com/Alice-Drop/Coding_Notes_Project/blob/main/LICENSE">CC BY-NC</a> 协议保护。</p>
    
-    <p style="font-size:15px;">本项目为个人项目,如果喜欢本网站，请给 <a class="paraLink" href="https://github.com/Alice-Drop/Coding_Notes_Project" target="_blank">本项目</a> 一个star，谢谢！</p>
+    <p style="font-size:15px;">本项目为个人项目,如果喜欢本网站，请给 <a href="https://github.com/Alice-Drop/Coding_Notes_Project" target="_blank">本项目</a> 一个star，谢谢！</p>
 `
 }
