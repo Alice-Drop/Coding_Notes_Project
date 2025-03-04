6505b4f9967094fc66e874a6cb5d11178a26eb8f
AliceDrop <2353663725@qq.com>
2025-03-04 22:51:54 +0800

    enhence appearence; update README

--- script/index.js
+++ script/index.js
@@ -22,13 +22,13 @@ function initIndex(){
     let greeting = "";
     let time_now = new Date();
     if (time_now.getHours() < 11){
-        greeting = "早上好呀\n今天想做点什么？";
+        greeting = "早上好呀 ,\n今天想做点什么？";
     }else if (time_now.getHours() < 14){
-        greeting = "中午好\n今天想做点什么？";
+        greeting = "中午好 ,\n今天想做点什么？";
     }else if (time_now.getHours() < 18){
-        greeting = "下午好\n今天想做点什么？"
+        greeting = "下午好 ,\n今天想做点什么？"
     }else if (time_now.getHours() < 24){
-        greeting = "晚上好\n今天想做点什么？"
+        greeting = "晚上好 ,\n今天想做点什么？"
     }
 
     let greeting_span = document.createElement("p");
